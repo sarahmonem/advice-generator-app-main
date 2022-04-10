aef57d2cf0f03f263c70c6cf8a5277e5243143d8
sarahmonem <sarahamonem@gmail.com>
2022-04-10 15:54:33 +0300

    edits

--- fetch.js
+++ fetch.js
@@ -1,13 +1,17 @@
 let adviceid = document.getElementById("advice-id")
 let advice = document.getElementById("adv")
-fetch("https://api.adviceslip.com/advice")
-  .then(function (res) {
-    return res.json()
-  })
-  .then((data) => {
-  console.log(data);
-let obj = data.slip
-console.log(obj);
-adviceid.innerHTML = obj.id
-advice.innerHTML = obj.advice
-  })
+
+
+  function getAdvice(){
+    fetch("https://api.adviceslip.com/advice")
+    .then(function (res) {
+      return res.json()
+    })
+    .then((data) => {
+    console.log(data);
+  let obj = data.slip
+  console.log(obj);
+  adviceid.innerHTML = obj.id
+  advice.innerHTML = obj.advice
+    })
+  }
